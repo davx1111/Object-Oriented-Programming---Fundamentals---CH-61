7483afb3cb42f308f439b2e4526ef85aeeebff55
David Carranco <david_carranco1111@outlook.es>
2025-11-27 16:59:04 -0600

    Le agrego 3 constructores a Student.java

--- Student.java
+++ Student.java
@@ -7,6 +7,26 @@ public class Student {
 	       int grade;
 	       int year;
 	       
+	       //Constructor 1 según ej.
+	       public Student (String firstName, String lastName, int registration, int grade, int year) { 
+	    	   this.firstName = firstName;
+	           this.lastName = lastName;
+	           this.registration = registration;
+	           this.grade = grade;
+	           this.year = year;
+	       }
+	    	   
+	       //Constructor 2 según ej 
+	       public Student(String firstName, String lastName, int registration) {
+	           this(firstName, lastName, registration, 0, 1);
+	       }
+	       
+	       //Constructor 3
+	       public Student(String firstName, String lastName) {
+	           this(firstName, lastName, 0, 0, 1);
+	       }
+
+	    	   
 	       public void printFullName(){
 	    	      System.out.println(firstName + " " + lastName);
 	    	   }
