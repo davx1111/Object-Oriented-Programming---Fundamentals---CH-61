0776ece6fdb43d9db37a100f1215d6fd40e96f64
David Carranco <david_carranco1111@outlook.es>
2025-11-28 08:40:00 -0600

    Se desarrollan los métodos que pedía ejericio, y corrige sintaxis en courses

--- Courses.java
+++ Courses.java
@@ -2,31 +2,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Courses {
-	
-	//Añado atributos que se me solicita
-	public class Course {
-	    String courseName;
-	    String professorName;
-	    int year;
-	    List<Student> students;
-	    
-	    public void enroll(Student student){
-	        //TODO add the student to the collection
-	     }
 
-	     public void unEnroll(Student student){
-	         //TODO remove this student from the collection
-	         // Hint: check if that really is this student
-	     }
+    // Añado atributos que se me solicita
+    String courseName;
+    String professorName;
+    int year;
+    List<Student> students; // añado también lista students
 
-	     public int countStudents(){
-	         //TODO implement
-	         return 0;
-	     }
-	     
-	     public int bestGrade(){
-	         //TODO implement
-	         return 0;
-	     }
+    // Constructor
+    public Courses(String courseName, String professorName, int year) {
+        this.courseName = courseName;
+        this.professorName = professorName;
+        this.year = year;
+        this.students = new ArrayList<>();
+    }
 
+    // Método inscripción de estudiante
+    public void enroll(Student student) {
+        students.add(student);
+        System.out.println(student.firstName + " " + student.lastName +
+                         " ha sido inscrito en " + courseName);
+    }
+
+    // Método para dar de baja a estudiantes
+    public void unEnroll(Student student) {
+        if (students.contains(student)) {
+            students.remove(student);
+            System.out.println(student.firstName + " " + student.lastName +
+                             " ha sido dado de baja en " + courseName);
+        } else {
+            System.out.println("No se encontró al estudiante en el curso");
+        }
+    }
+
+    public int countStudents() {
+        return students.size();
+    }
+
+    // Encontrar la mejor calificación
+    public int bestGrade() {
+        int best = 0;
+        for (Student student : students) {
+            if (student.grade > best) {
+                best = student.grade;
+            }
+        }
+        return best;
+    }
 }
